ca00caba9aec61a8e4a17808f5254f0b43928ae1
trinm31 <trinmgcd19833@fpt.edu.vn>
2021-12-14 10:30:48 +0700

    lay data from db

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ORM_EFcore.DataAccess;
 using ORM_EFcore.Models;
 
 namespace ORM_EFcore.Controllers
@@ -12,15 +13,18 @@ namespace ORM_EFcore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ORMEFCoreContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ORMEFCoreContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var StudentList = _db.Students.ToList();
+            return View(StudentList);
         }
 
         public IActionResult Privacy()
